47c4e219f358c809ed7073fab09a2ecea49298e1
Andrew Bernklau <andrewbernklau@hotmail.com>
2022-05-18 20:27:07 -0600

    Added last bit of securityt packages

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use(express.json());
+
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {});
 
