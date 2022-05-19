7b2205a71408f12ab96c402dd2e25b27d8cf02b3
Andrew Bernklau <andrewbernklau@hotmail.com>
2022-05-18 20:01:54 -0600

    removed white-space

--- client/src/components/FormInput.js
+++ client/src/components/FormInput.js
@@ -2,7 +2,7 @@ const FormInput = (props) => {
   return (
     <div className="form-row">
       <label htmlFor={props.name} className="form-label">
-        {props.labelText || props.name}{" "}
+        {props.labelText || props.name}
         {props.isStarred && <span className="starred">*</span>}
       </label>
       <input
